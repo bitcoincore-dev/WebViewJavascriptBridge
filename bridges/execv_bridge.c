f051e30ab0a07ea49491f64db03681705d1daa4f
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-12-03 21:31:00 -0500

    bridges/execv_bridge.c:clean up and self referencing loop working

--- bridges/execv_bridge.c
+++ bridges/execv_bridge.c
@@ -1,19 +1,23 @@
-//
-//  execv_bridge.c
-//  AppWithTool
-//
-//  Created by git on 5/18/23.
-//
-
 #include "execv_bridge.h"
-
+#include <string.h>
 int execv_bridge(const char *path, char *const argv[]){
-    
-    printf("execv_bridge:\n");
-    printf("path:%s\n", path);
-    printf("*argv:%s\n", *argv);
-    execv(path, argv);
-    
-    return 0;//execv(path, argv);
-    
+
+    if (DEBUG){
+
+        printf("-----------------execv_bridge-----------------\n");
+
+        printf("*argv:%s\n", *argv);
+        printf("*path:%c\n", *path);
+        printf(" path:%s\n",  path);
+
+        if ((int *)strlen(*argv) > 0){
+
+            printf("(int *)strlen(*argv) = %d\n", (int *)strlen(*argv));
+            printf("*argv = %s\n", *argv);
+
+        }
+
+    }
+    return execv(*argv, argv);
+
 }
